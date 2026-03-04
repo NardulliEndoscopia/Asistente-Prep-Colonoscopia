42172b8192045b46af886aad6cb7053fe597aff6
NardulliEndoscopia <endoscopianardulli@gmail.com>
2026-03-04 20:00:57 +0100

    Initialize Express server with static file serving

---
+++ server.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+// Servir los archivos estáticos de tu aplicación
+app.use(express.static(path.join(__dirname, 'dist')));
+app.use(express.static(__dirname));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'index.html'));
+});
+
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Servidor activo en puerto ${PORT}`);
+});
